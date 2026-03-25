99adf02afea546f432978dde7508e98743c0e56d
Gaurav Saini <me@gauravsaini.com>
2026-03-25 15:19:32 +1100

    Add generated statechart integration tests

---
+++ nivasa-statechart/tests/generated_statecharts.rs
@@ -0,0 +1,334 @@
+use nivasa_statechart::codegen;
+use nivasa_statechart::parser::ScxmlDocument;
+use nivasa_statechart::{
+    GENERATED_STATECHARTS, StatechartEngine,
+    NivasaApplicationEvent, NivasaApplicationState, NivasaApplicationStatechart,
+    NivasaModuleEvent, NivasaModuleState, NivasaModuleStatechart,
+    NivasaProviderEvent, NivasaProviderState, NivasaProviderStatechart,
+    NivasaRequestEvent, NivasaRequestState, NivasaRequestStatechart,
+};
+use std::path::PathBuf;
+
+fn statecharts_dir() -> PathBuf {
+    PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("../statecharts")
+}
+
+fn drive<S>(engine: &mut StatechartEngine<S>, steps: &[(S::Event, S::State)])
+where
+    S: nivasa_statechart::StatechartSpec,
+    S::State: Copy + std::fmt::Debug + PartialEq,
+    S::Event: Clone + std::fmt::Debug,
+{
+    for (event, expected) in steps {
+        let observed = engine.send_event(event.clone()).unwrap();
+        assert_eq!(observed, *expected);
+        assert_eq!(engine.current_state(), *expected);
+    }
+}
+
+#[test]
+fn generated_registry_matches_source_scxml_hashes() {
+    for generated in GENERATED_STATECHARTS {
+        let path = statecharts_dir().join(generated.file_name);
+        let doc = ScxmlDocument::from_file(&path).unwrap();
+
+        assert_eq!(
+            doc.content_hash(),
+            generated.scxml_hash,
+            "hash mismatch for {}",
+            generated.file_name,
+        );
+
+        let rendered = codegen::generate_rust_with_spec_path(&doc, "crate");
+        assert!(
+            rendered.contains(&format!("pub struct {}Statechart;", generated.name)),
+            "missing generated statechart type for {}",
+            generated.file_name,
+        );
+        assert!(
+            rendered.contains(generated.scxml_hash),
+            "missing embedded hash for {}",
+            generated.file_name,
+        );
+    }
+}
+
+#[test]
+fn application_bootstrap_path_reaches_running() {
+    let mut engine = StatechartEngine::<NivasaApplicationStatechart>::new(
+        NivasaApplicationState::ResolvingModules,
+    );
+
+    assert_eq!(
+        engine.valid_events(),
+        vec![
+            NivasaApplicationEvent::ModulesResolved,
+            NivasaApplicationEvent::ErrorModuleCircularDependency,
+        ],
+    );
+
+    drive(
+        &mut engine,
+        &[
+            (
+                NivasaApplicationEvent::ModulesResolved,
+                NivasaApplicationState::InitializingModules,
+            ),
+            (
+                NivasaApplicationEvent::ModulesInitialized,
+                NivasaApplicationState::RegisteringRoutes,
+            ),
+            (
+                NivasaApplicationEvent::RoutesRegistered,
+                NivasaApplicationState::ApplyingGlobalConfig,
+            ),
+            (
+                NivasaApplicationEvent::ConfigApplied,
+                NivasaApplicationState::BootstrapComplete,
+            ),
+            (
+                NivasaApplicationEvent::AppStart,
+                NivasaApplicationState::Running,
+            ),
+        ],
+    );
+
+    assert!(!engine.is_in_final_state());
+}
+
+#[test]
+fn application_failure_path_reaches_terminated() {
+    let mut engine = StatechartEngine::<NivasaApplicationStatechart>::new(
+        NivasaApplicationState::ResolvingModules,
+    );
+
+    drive(
+        &mut engine,
+        &[
+            (
+                NivasaApplicationEvent::ErrorModuleCircularDependency,
+                NivasaApplicationState::BootstrapFailed,
+            ),
+            (
+                NivasaApplicationEvent::AppAbort,
+                NivasaApplicationState::Terminated,
+            ),
+        ],
+    );
+
+    assert!(engine.is_in_final_state());
+}
+
+#[test]
+fn module_happy_path_reaches_destroyed() {
+    let mut engine = StatechartEngine::<NivasaModuleStatechart>::new(
+        NivasaModuleState::ResolvingImports,
+    );
+
+    drive(
+        &mut engine,
+        &[
+            (
+                NivasaModuleEvent::ImportsResolved,
+                NivasaModuleState::RegisteringProviders,
+            ),
+            (
+                NivasaModuleEvent::ProvidersRegistered,
+                NivasaModuleState::ResolvingDependencies,
+            ),
+            (
+                NivasaModuleEvent::DependenciesResolved,
+                NivasaModuleState::Loaded,
+            ),
+            (
+                NivasaModuleEvent::ModuleInit,
+                NivasaModuleState::Initialized,
+            ),
+            (
+                NivasaModuleEvent::ModuleActivate,
+                NivasaModuleState::Active,
+            ),
+            (
+                NivasaModuleEvent::ModuleDestroy,
+                NivasaModuleState::Destroying,
+            ),
+            (
+                NivasaModuleEvent::DestroyComplete,
+                NivasaModuleState::Destroyed,
+            ),
+        ],
+    );
+
+    assert!(engine.is_in_final_state());
+}
+
+#[test]
+fn module_load_failure_short_circuits_to_failed() {
+    let mut engine = StatechartEngine::<NivasaModuleStatechart>::new(
+        NivasaModuleState::ResolvingImports,
+    );
+
+    drive(
+        &mut engine,
+        &[
+            (
+                NivasaModuleEvent::ErrorImportMissing,
+                NivasaModuleState::LoadFailed,
+            ),
+            (
+                NivasaModuleEvent::ModuleAbort,
+                NivasaModuleState::Failed,
+            ),
+        ],
+    );
+
+    assert!(engine.is_in_final_state());
+}
+
+#[test]
+fn provider_happy_path_reaches_disposed() {
+    let mut engine = StatechartEngine::<NivasaProviderStatechart>::new(
+        NivasaProviderState::Unregistered,
+    );
+
+    drive(
+        &mut engine,
+        &[
+            (
+                NivasaProviderEvent::ProviderRegister,
+                NivasaProviderState::Registered,
+            ),
+            (
+                NivasaProviderEvent::ProviderResolve,
+                NivasaProviderState::Resolving,
+            ),
+            (
+                NivasaProviderEvent::DependenciesReady,
+                NivasaProviderState::Constructing,
+            ),
+            (
+                NivasaProviderEvent::ProviderConstructed,
+                NivasaProviderState::Resolved,
+            ),
+            (
+                NivasaProviderEvent::ProviderDispose,
+                NivasaProviderState::Disposing,
+            ),
+            (
+                NivasaProviderEvent::ProviderDisposed,
+                NivasaProviderState::Disposed,
+            ),
+        ],
+    );
+
+    assert!(engine.is_in_final_state());
+}
+
+#[test]
+fn provider_resolution_failure_short_circuits_to_failed() {
+    let mut engine = StatechartEngine::<NivasaProviderStatechart>::new(
+        NivasaProviderState::Resolving,
+    );
+
+    drive(
+        &mut engine,
+        &[
+            (
+                NivasaProviderEvent::ErrorDependencyMissing,
+                NivasaProviderState::ResolutionFailed,
+            ),
+            (
+                NivasaProviderEvent::ProviderAbort,
+                NivasaProviderState::Failed,
+            ),
+        ],
+    );
+
+    assert!(engine.is_in_final_state());
+}
+
+#[test]
+fn request_happy_path_reaches_done() {
+    let mut engine = StatechartEngine::<NivasaRequestStatechart>::new(
+        NivasaRequestState::Received,
+    );
+
+    assert_eq!(
+        engine.valid_events(),
+        vec![
+            NivasaRequestEvent::RequestParsed,
+            NivasaRequestEvent::ErrorParse,
+        ],
+    );
+
+    drive(
+        &mut engine,
+        &[
+            (
+                NivasaRequestEvent::RequestParsed,
+                NivasaRequestState::MiddlewareChain,
+            ),
+            (
+                NivasaRequestEvent::MiddlewareComplete,
+                NivasaRequestState::RouteMatching,
+            ),
+            (
+                NivasaRequestEvent::RouteMatched,
+                NivasaRequestState::GuardChain,
+            ),
+            (
+                NivasaRequestEvent::GuardsPassed,
+                NivasaRequestState::InterceptorPre,
+            ),
+            (
+                NivasaRequestEvent::InterceptorsPreComplete,
+                NivasaRequestState::PipeTransform,
+            ),
+            (
+                NivasaRequestEvent::PipesComplete,
+                NivasaRequestState::HandlerExecution,
+            ),
+            (
+                NivasaRequestEvent::HandlerComplete,
+                NivasaRequestState::InterceptorPost,
+            ),
+            (
+                NivasaRequestEvent::InterceptorsPostComplete,
+                NivasaRequestState::SendingResponse,
+            ),
+            (
+                NivasaRequestEvent::ResponseSent,
+                NivasaRequestState::Done,
+            ),
+        ],
+    );
+
+    assert!(engine.is_in_final_state());
+}
+
+#[test]
+fn request_guard_denied_short_circuits_to_done() {
+    let mut engine = StatechartEngine::<NivasaRequestStatechart>::new(
+        NivasaRequestState::GuardChain,
+    );
+
+    drive(
+        &mut engine,
+        &[
+            (
+                NivasaRequestEvent::GuardDenied,
+                NivasaRequestState::ErrorHandling,
+            ),
+            (
+                NivasaRequestEvent::FilterHandled,
+                NivasaRequestState::SendingResponse,
+            ),
+            (
+                NivasaRequestEvent::ErrorSend,
+                NivasaRequestState::Done,
+            ),
+        ],
+    );
+
+    assert!(engine.is_in_final_state());
+}
