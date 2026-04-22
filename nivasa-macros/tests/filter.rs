56845cbbf882bbad2e62c135d1ac34e58ae57b84
Gaurav Saini <me@gauravsaini.com>
2026-04-22 23:29:32 +1000

    Add shared macro surface coverage tests

--- nivasa-macros/tests/filter.rs
+++ nivasa-macros/tests/filter.rs
@@ -1,4 +1,36 @@
+use nivasa_filters::ExceptionFilterMetadata;
+use nivasa_macros::{catch, catch_all};
 use trybuild::TestCases;
+
+struct HttpException;
+
+#[catch(HttpException)]
+struct HttpExceptionFilter;
+
+#[catch_all]
+struct DefaultExceptionFilter;
+
+#[test]
+fn filter_macros_emit_helper_surface() {
+    let typed_filter = HttpExceptionFilter;
+    let catch_all_filter = DefaultExceptionFilter;
+
+    assert_eq!(
+        HttpExceptionFilter::__nivasa_filter_exception(),
+        "HttpException"
+    );
+    assert_eq!(
+        HttpExceptionFilter::__nivasa_filter_exception_type(),
+        std::any::type_name::<HttpException>()
+    );
+    assert_eq!(
+        typed_filter.exception_type(),
+        Some(std::any::type_name::<HttpException>())
+    );
+
+    assert!(DefaultExceptionFilter::__nivasa_filter_catch_all());
+    assert!(catch_all_filter.is_catch_all());
+}
 
 #[test]
 fn filter_macro_validation() {
